e9560a2ecee32dbe8448eb49a297f14ee6d6e022
Matt Jones <matt.jones@mediabeacon.com>
2019-04-29 21:59:41 -0500

    variable for host name, read from external properties file

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+host=upstairs_thermostat
 
-ssh -t pi@192.168.0.24 -o StrictHostKeyChecking=no "git clone https://github.com/pimoroni/bme680"
-ssh -t pi@192.168.0.24 -o StrictHostKeyChecking=no "sudo python bme680/library/setup.py install"
-ssh -t pi@192.168.0.24 -o StrictHostKeyChecking=no "mkdir /home/pi/com"
-ssh -t pi@192.168.0.24 -o StrictHostKeyChecking=no "mkdir /home/pi/com/bigboxer23"
-ssh -t pi@192.168.0.24 -o StrictHostKeyChecking=no "mkdir /home/pi/com/bigboxer23/climate-service"
-ssh -t pi@192.168.0.24 -o StrictHostKeyChecking=no "mkdir /home/pi/com/bigboxer23/climate-service/1.0.0"
-ssh -t pi@192.168.0.24 -o StrictHostKeyChecking=no "cp /home/pi/bme680/examples/read-all.py /home/pi/com/bigboxer23/climate-service/1.0.0"
-scp -o StrictHostKeyChecking=no -r keystore.p12 pi@homecontroller:/home/pi/
+ssh -t pi@$host -o StrictHostKeyChecking=no "git clone https://github.com/pimoroni/bme680"
+ssh -t pi@$host -o StrictHostKeyChecking=no "sudo python bme680/library/setup.py install"
+ssh -t pi@$host -o StrictHostKeyChecking=no "mkdir /home/pi/com"
+ssh -t pi@$host -o StrictHostKeyChecking=no "mkdir /home/pi/com/bigboxer23"
+ssh -t pi@$host -o StrictHostKeyChecking=no "mkdir /home/pi/com/bigboxer23/climate-service"
+ssh -t pi@$host -o StrictHostKeyChecking=no "mkdir /home/pi/com/bigboxer23/climate-service/1.0.0"
+ssh -t pi@$host -o StrictHostKeyChecking=no "cp /home/pi/bme680/examples/read-all.py /home/pi/com/bigboxer23/climate-service/1.0.0"
+scp -o StrictHostKeyChecking=no -r keystore.p12 pi@$host:/home/pi/
 
 #Still have to install these manually for now...
 #cd /home/pi
