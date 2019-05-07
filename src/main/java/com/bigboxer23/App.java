3f91a00cab2027b5cba458368b8ba557b21a0092
Matt Jones <matt.jones@mediabeacon.com>
2019-05-06 21:26:25 -0500

    Push content to openHAB directly, if defined.

--- src/main/java/com/bigboxer23/App.java
+++ src/main/java/com/bigboxer23/App.java
@@ -2,11 +2,13 @@ package com.bigboxer23;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Hello climate!
  */
 @SpringBootApplication
+@EnableScheduling
 public class App
 {
 	public static void main(String[] args)
--- src/main/java/com/bigboxer23/ClimateController.java
+++ src/main/java/com/bigboxer23/ClimateController.java
@@ -1,13 +1,21 @@
 package com.bigboxer23;
 
 import com.bigboxer23.climate_service.sensor.BME680Controller;
+import com.bigboxer23.util.http.HttpClientUtils;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 /**
@@ -18,6 +26,15 @@ import java.util.Map;
 public class ClimateController
 {
 	private static final Logger myLogger = LoggerFactory.getLogger(ClimateController.class);
+
+	/**
+	 * Location of OpenHAB
+	 */
+	@Value("${openHABUrl}")
+	private String kOpenHABUrl;
+
+	@Value("${sensorName}")
+	private String kSensorName;
 
 	private BME680Controller myBME680Controller;
 
@@ -37,5 +54,36 @@ public class ClimateController
 	{
 		myLogger.debug("Climate data requested");
 		return myBME680Controller.getClimateData();
+	}
+
+	@Scheduled(fixedDelay = 30000)
+	private void sendClimateData()
+	{
+		if (kOpenHABUrl == null || kSensorName == null)
+		{
+			return;
+		}
+		myBME680Controller.getClimateData().forEach((theData, theValue) ->
+		{
+			HttpPost aHttpPost = new HttpPost(kOpenHABUrl + "/rest/items/" + kSensorName + capitalizeFirstLetter(theData));
+			try
+			{
+				aHttpPost.setEntity(new ByteArrayEntity(URLDecoder.decode("" + theValue, StandardCharsets.UTF_8.displayName()).getBytes(StandardCharsets.UTF_8)));
+			}
+			catch (UnsupportedEncodingException theE)
+			{
+				myLogger.warn("OpenHABController:doAction", theE);
+			}
+			HttpClientUtils.execute(aHttpPost);
+		});
+	}
+
+	private String capitalizeFirstLetter(String theString)
+	{
+		if (theString == null || theString.length() == 0)
+		{
+			return theString;
+		}
+		return theString.substring(0, 1).toUpperCase() + theString.substring(1);
 	}
 }
